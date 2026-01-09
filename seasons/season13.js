a0f8c6d8bae0656ed6f54d7e0f7b62921a5277f1
lobos-gotc <charmketchup@gmail.com>
2026-01-08 20:01:19 -0800

    Add Season 13 with Silver Ore material
    - Created season13 folder for materials
    - Added Silver Ore material to materials.js
    - Created season13.js placeholder for future gear sets
    - Added season 13 to seasonsMap in products.js
    - Included season13.js in index.html
    
    Note: Silver Ore image (silver-ore.png) needs to be added to
    resources/materials/season13/ directory.

---
+++ seasons/season13.js
@@ -0,0 +1,8 @@
+const season13 = {
+	season: 13,
+	extraLevels: {30: 3000, 35: 12000, 40: 45000, 45: 120000},
+	sets: [
+		// Season 13 gear sets will be added here when available
+		// Material: Silver Ore
+	]
+};
